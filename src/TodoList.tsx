857776493f229995b9c48d86003a40ba5738ac63
jiyoonZ <wldbsl416@gmail.com>
2022-11-15 18:35:11 +0900

    유효성 검사

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,22 +2,29 @@ import React, {useState} from "react";
 
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (evt: React.FormEvent<HTMLInputElement>) => {
     // const {value} = evt.currentTarget;
     const {
       currentTarget: {value},
     } = evt;
     setToDo(value);
+    setToDoError("");
   };
   const onSubmint = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     console.log(toDo);
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit", toDoError);
   };
   return (
     <div>
       <form onSubmit={onSubmint}>
         <input onChange={onChange} value={toDo} placeholder="Write a todo" />
         <button>Add</button>
+        {toDoError !== "" ? toDoError : ""}
       </form>
     </div>
   );
