bf9eaa481e7e828274d15cfba4f3f1281577477f
jiyoonZ <wldbsl416@gmail.com>
2022-11-17 01:55:03 +0900

    카테고리 이동 동적으로

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {useSetRecoilState} from "recoil";
-import {ITodo, todoState} from "../atoms";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {categoriesState, ITodo, todoState} from "../atoms";
 
 // 1. target 의 경로찾기 id로 수정할 todo 찾기 [index 찾기]
 // 2. 새로운 todo 를 만들어서 수정하기
 function ToDo({text, category, id}: ITodo) {
   const setTodos = useSetRecoilState(todoState);
+  const categories = useRecoilValue(categoriesState);
   const clickHandler = (evt: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: {name},
@@ -29,21 +30,13 @@ function ToDo({text, category, id}: ITodo) {
   return (
     <li>
       <span>{text} </span>
-      {category !== "TODO" && (
-        <button name="TODO" onClick={clickHandler}>
-          To do
-        </button>
-      )}
-      {category !== "DOING" && (
-        <button name="DOING" onClick={clickHandler}>
-          Doing
-        </button>
-      )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={clickHandler}>
-          Done
-        </button>
-      )}
+      {categories.map((ele) => {
+        return (
+          <button disabled={ele === category} name={ele} onClick={clickHandler}>
+            {ele}
+          </button>
+        );
+      })}
     </li>
   );
 }
