06c35e3d3e137a2d82a336326068ae0abdb626f3
jiyoonZ <wldbsl416@gmail.com>
2022-11-17 00:04:29 +0900

    recoil-persist 적용하기

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,19 +1,37 @@
 import {atom, selector} from "recoil";
+import {recoilPersist} from "recoil-persist";
+// type categories = "TODO" | "DOING" | "DONE";
 
+// enum 은 문자열이 아닌 일련의 숫자를 부여함
+// 따라서 실제값은 "TODO" 가 아닌 1
+// 데이터를 유의미하게 주고싶다면
+// "TODO" = "TODO"
+export enum Categories {
+  "TODO" = "TODO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
 export interface ITodo {
   id: number;
   text: string;
-  category: "TODO" | "DOING" | "DONE";
+  category: Categories;
 }
 
-export const categoryState = atom({
+const {persistAtom} = recoilPersist({
+  key: "todosList",
+  storage: localStorage,
+});
+
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TODO",
+  default: Categories.TODO,
+  //defailt : "TODO"
 });
 //atom 생성
 export const todoState = atom<ITodo[]>({
   key: "toDo",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 //selector
