4389bae718852a027aafcce15747467ffaf736cb
jiyoonZ <wldbsl416@gmail.com>
2023-02-25 13:24:31 +0900

    Fix : 타입 분리 및 코드 정리

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,38 +1,22 @@
 import {atom} from "recoil";
 import {recoilPersist} from "recoil-persist";
+import {ITodoList} from "./type";
 
 const {persistAtom} = recoilPersist({
   key: "todosList",
   storage: localStorage,
 });
-interface ITodoState {
-  [key: string]: ITodo[];
-}
-export interface ITodo {
-  id: number;
-  text: string;
-}
-export interface IForm {
-  board: string;
+
+const DEFAULT_CATEGORY = {
+  "TODO":[], 
+  "DOING":[], 
+  "DONE":[]
 }
 
-localStorage.setItem("categories", JSON.stringify({"TODO":[], "DOING":[], "DONE":[]}))
-const defaultCategories = JSON.parse(
-  localStorage.getItem("categories") as string
-);
-
-export const categoriesState = atom<string[]>({
-  key: "categories",
-  default: defaultCategories
-    ? defaultCategories
-    : {TODO: [], DOING: [], DONE: []},
-});
-
-
-export const todoState = atom<ITodoState>({
+export const todoState = atom<ITodoList>({
   key: "todo",
   default: {
-    ...defaultCategories,
+    ...DEFAULT_CATEGORY,
   },
   effects_UNSTABLE: [persistAtom],
 });
